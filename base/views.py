b3b64e8a84c77a51223ba519c6eae9ebec637006
Martin Púchly <martin.puchly@gmail.com>
2023-03-13 19:32:43 +0100

    opravy

--- base/views.py
+++ base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from pages.models import QuickNew, Post
+from datetime import datetime
+from django.db.models import Q
+
 
 def home_view(request):
     return render(request, "home.html", {
         'qicknews': QuickNew.objects.all()[:5],
-        'posts': Post.objects.order_by('-created_at').all()[:3]
+        'posts': Post.objects.order_by('-created_at').filter(Q(publised__exact=1) | Q(publised_at__lte=datetime.now())).all()[:3]
     })
 
 
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Page, Post
 from django.shortcuts import get_object_or_404, render
-
-
+from django.db.models import Q
+from datetime import datetime
 def page_show(request, page_slug):
     page = get_object_or_404(Page, slug=page_slug)
     return render(request, "pages/show.html", {
@@ -11,13 +11,13 @@ def page_show(request, page_slug):
 
 
 def post_index(request):
-    posts = Post.objects.order_by('-created_at').all()[:5]
+    posts = Post.objects.order_by('-created_at').filter(Q(publised__exact=1) | Q(publised_at__lte=datetime.now())).all()[:5]
     return render(request, "posts/index.html", {
         'posts': posts
     })
 
 def post_tag_index(request, tag):
-    posts = Post.objects.filter(tags__slug=tag).all()[:5]
+    posts = Post.objects.filter(Q(publised__exact=1) | Q(publised_at__lte=datetime.now())).filter(tags__slug=tag).all()[:5]
     return render(request, "posts/index.html", {
         'posts': posts
     })
